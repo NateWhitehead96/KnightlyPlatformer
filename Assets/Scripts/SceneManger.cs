aac0506271079f46857393d126c4f689b2cfac49
NateWhitehead96 <n_whitehead96@hotmail.ca>
2020-11-17 10:43:27 -0500

    All Scenes in
    All scenes are now in the game. Close to complete for assignment 1

---
+++ Assets/Scripts/SceneManger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManger : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
