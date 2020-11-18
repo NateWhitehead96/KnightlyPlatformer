d5024126cc92861e01b37d7a635bbe5a7934c523
NateWhitehead96 <n_whitehead96@hotmail.ca>
2020-11-18 11:28:50 -0500

    Final update for assignment1
    Implemented basic controls for switching scenes, and player movement. Added sounds for buttons and music to all levels.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer sprite;
+
+    public Joystick joystick;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
+    public float horizontalForce;
+    public float verticalForce;
+    public bool grounded;
+    public bool jumping = false;
+    public bool attacking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (grounded)
+        {
+            if (!attacking)
+            {
+                if (joystick.Horizontal > joystickHorizontalSensitivity)
+                {
+                    // move right
+                    rigidbody.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                    //sprite.flipX = false;
+                }
+                else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+                {
+                    // move left
+                    rigidbody.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                    //sprite.flipX = true;
+                }
+            }
+            rigidbody.velocity *= 0.9f;
+        }
+    }
+
+    public void AButton()
+    {
+        // Attack button, but for now will switch to win scene
+        SceneManager.LoadScene("Win");
+    }
+
+    public void BButton()
+    {
+        // Jump button
+        rigidbody.AddForce(Vector2.up * verticalForce * Time.deltaTime);
+        jumping = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) // if colliding with ground we're grounded and not jumping
+        {
+            grounded = true;
+            jumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Hazard"))
+        {
+            // fell on spikes right now will 1 shot you
+            SceneManager.LoadScene("Lose");
+        }
+    }
+}
--- Assets/Scripts/SceneManger.cs
+++ Assets/Scripts/SceneManger.cs
@@ -5,23 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneManger : MonoBehaviour
 {
+    public AudioSource buttonSound;
     public void Back()
     {
+        buttonSound.Play();
         SceneManager.LoadScene("MainMenu");
     }
 
     public void HowToPlay()
     {
+        buttonSound.Play();
         SceneManager.LoadScene("HowToPlay");
     }
 
     public void Play()
     {
+        buttonSound.Play();
         SceneManager.LoadScene("Game");
     }
 
     public void Quit()
     {
+        buttonSound.Play();
         Application.Quit();
     }
 }
